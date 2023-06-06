c6724470ab97f9dcc2fc29e77cd954fc63582bf1
Kristofer Valerio <krisvalerio02@techexchange.in>
2023-06-06 17:45:35 +0000

    When a user has collection configured, the collection page dynamically displays that data

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,4 +1,6 @@
 import hashlib
+import json
+import csv
 
 '''
 Backend contains methods used for various under-the-cover functions,
@@ -30,11 +32,19 @@ class Backend:
         '''
         prefixed_password = 'nba_wiki_game' + password
         hashed_password = hashlib.sha256(prefixed_password.encode()).hexdigest()
-        blob = self.bucket.blob(username)
-        if blob.exists():
+        password_blob = self.bucket.blob(f"{username}_password")
+        if password_blob.exists():
             return False
         else:
-            blob.upload_from_string(hashed_password)
+            password_blob.upload_from_string(hashed_password)
+            
+# Initialize dictionary which will holds user's collection (player ID as key and value is medal string)
+            collection = dict()
+# Convert dictionary to JSON string
+            json_data = json.dumps(collection)
+# Upload collection blob
+            collection_blob = self.bucket.blob(f"{username}_collection")
+            collection_blob.upload_from_string(json_data)
             return True
 
     def log_in(self, username, password):
@@ -44,14 +54,68 @@ class Backend:
         '''
         prefixed_password = 'nba_wiki_game' + password
         hashed_password = hashlib.sha256(prefixed_password.encode()).hexdigest()
-        blob = self.bucket.blob(username)
-        if blob.exists():
+        password_blob = self.bucket.blob(f"{username}_password")
+        if password_blob.exists():
             # Access stored password (associated to username) and compare inputted password
-            bucket_password = blob.download_as_bytes().decode('utf-8')
+            bucket_password = password_blob.download_as_bytes().decode('utf-8')
             if hashed_password == bucket_password:
                 return True
         return False
 
+    def get_player_collection(self, username):
+        '''
+        Method will access the user's collection which should consist of a dictionary,
+        keys are player ID's they've collected and value is a tuple, index 0 = medal and index 1 = tries
+        '''
+        collection_blob = self.bucket.blob(f"{username}_collection")
+# Get the collection (json data) from the collection blob, then convert that json_data into a functional dict
+        json_data = collection_blob.download_as_string().decode('utf-8')
+        json_dict = json.loads(json_data)
+        collection = dict()
+        for key in json_dict:
+            collection[int(key)] = json_dict[key]
+        return collection
+
+    def test_collection(self, username):
+        collection_blob = self.bucket.blob(f"{username}_collection")
+        json_data = collection_blob.download_as_string().decode('utf-8')
+        collection = json.loads(json_data)
+
+# FOR NOW, WE WILL SET USER LEBRON INFO TO {0: [Gold, 3], which is steph and gold medal
+        collection[0] = ['Gold', 3]
+        collection[2] = ['Silver', 6]
+
+# Convert collection back into json data
+        updated_json_data = json.dumps(collection)
+    
+# Upload the updated JSON string as the content of the blob
+        collection_blob.upload_from_string(updated_json_data)
+
+        return
+
+    
+    def read_csv_file(self, file_path):
+        '''
+        Method reads and parses csv file
+        '''
+        data = []
+        with open(file_path, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            for row in csvreader:
+                data.append(row)
+        return data
+    
+    def generate_links(self, cards_data):
+        '''
+        Method generates links for all NBA player images using the players names
+        '''
+        links = list()
+        for player_id in range(len(cards_data)):
+            # Converts player name into all lowercase, then splits the string by spaces into a list (Stephen Curry -> ['stephen', 'curry'])
+            temp = cards_data[player_id][0].lower().split()
+            links.append('https://storage.cloud.google.com/nba-players/' + temp[0] + '_' + temp[1] + '_image.png?authuser=3')
+        
+        return links
+
     def get_image(self):
         pass
-
--- flaskr/pages.py
+++ flaskr/pages.py
@@ -110,8 +110,65 @@ def make_endpoints(app, bucket_client):
         value = request.cookies.get('value')
         username = request.cookies.get('username')
         welcome = True
+        message = ''
+
+        backend = Backend('nba-user-credentials', bucket_client)
+
+# Access the user collection
+        collection = backend.get_player_collection(username)
+        list_of_player_collection = []
+        for player in collection:
+            list_of_player_collection.append(int(player))
+        print(list_of_player_collection)
+
+# Access player_information.csv
+        cards_data = backend.read_csv_file('flaskr/player_information.csv')
+
+# Generate links for every player within a dictionary
+        player_image_links = backend.generate_links(cards_data)
+
+# Create message, if collection is empty then state that it is
+        if len(collection) == 0:
+            message = 'Your collection is currently empty, click on play to collect cards!'
+
         resp = make_response(
             render_template("collection.html",
+                                value = value,
+                                username = username,
+                                welcome = welcome,
+                                message = message,
+                                cards_data = cards_data,
+                                collection = collection,
+                                player_image_links = player_image_links))
+        resp.set_cookie('welcome', '', expires = 0)
+        return resp
+    
+    @app.route("/nba22_23")
+    def nba22_23():
+        value = request.cookies.get('value')
+        username = request.cookies.get('username')
+        welcome = True
+
+        # Access player_information.csv and parse the info. It will be used here
+
+    
+        # Create backend instance, accessing 'nba-user-credentials' bucket
+        backend = Backend('nba-user-credentials', bucket_client)
+        collection = backend.get_player_collection(username)
+
+        backend.test_collection(username)
+        
+        backend.get_player_collection(username)
+
+        print('HERES PAGES, COLLECTION:')
+        print(collection)
+
+        csv_file = 'flaskr/player_information.csv'
+        cards_data = backend.read_csv_file(csv_file)
+        print(cards_data[0][0])
+
+        resp = make_response(
+            render_template("nba22_23.html",
                                 value = value,
                                 username = username,
                                 welcome = welcome))
